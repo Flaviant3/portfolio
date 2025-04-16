b842f08a62d2eab1645c9dbfd4952684b881849f
Github MMI3 <github@mmi-troyes.fr>
2025-04-16 13:51:27 +0000

    fix: image not showed

--- portfolio/src/Controller/ProjectController.php
+++ portfolio/src/Controller/ProjectController.php
@@ -2,91 +2,34 @@
 
 namespace App\Controller;
 
-use App\Entity\Project;
-use App\Form\ProjectType;
-use App\Repository\ProjectRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\ProjectService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProjectController extends AbstractController
 {
-    // Route pour afficher la liste des projets
     #[Route('/projets', name: 'projects')]
-    public function index(ProjectRepository $projectRepository): Response
+    public function index(ProjectService $projectService): Response
     {
-        $projects = $projectRepository->findAll();
+        $projects = $projectService->getAllProjects();
 
         return $this->render('project/index.html.twig', [
             'projects' => $projects,
         ]);
     }
-// Route pour afficher les détails d'un projet
-    #[Route('/projet/{id}', name: 'show_project')]
-    public function show(int $id, ProjectRepository $projectRepository): Response
-    {
-        // Récupérer le projet par son id
-        $project = $projectRepository->find($id);
 
-        // Si le projet n'existe pas, afficher une erreur 404
+    #[Route('/projet/{slug}', name: 'show_project')]
+    public function show(string $slug, ProjectService $projectService): Response
+    {
+        $project = $projectService->getProjectBySlug($slug);
+
         if (!$project) {
-            throw $this->createNotFoundException('Le projet demandé n\'existe pas.');
+            throw $this->createNotFoundException('Projet introuvable');
         }
 
         return $this->render('project/show.html.twig', [
             'project' => $project,
-        ]);
-    }
-
-
-    // Route pour ajouter un projet
-    #[Route('/ajouter-projet', name: 'add_project')]
-    public function add(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
-    {
-        $project = new Project();
-        $form = $this->createForm(ProjectType::class, $project);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var UploadedFile $imageFile */
-            $imageFile = $form->get('image')->getData();
-
-            // Si un fichier image a été téléchargé
-            if ($imageFile) {
-                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-                $safeFilename = $slugger->slug($originalFilename); // Crée un nom de fichier sans espace
-                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
-
-                try {
-                    // Déplace le fichier dans le répertoire spécifié
-                    $imageFile->move(
-                        $this->getParameter('uploads_directory'), // Paramètre qui définit le répertoire de destination
-                        $newFilename
-                    );
-                } catch (FileException $e) {
-                    throw new \Exception('Erreur lors de l\'upload du fichier');
-                }
-
-                // Associe le nom du fichier à l'entité project
-                $project->setImage($newFilename);
-            }
-
-            // Persiste l'entité project dans la base de données
-            $entityManager->persist($project);
-            $entityManager->flush();
-
-            // Message flash de succès
-            $this->addFlash('success', 'Le projet a été ajouté avec succès !');
-            return $this->redirectToRoute('projects');
-        }
-
-        return $this->render('project/add.html.twig', [
-            'form' => $form->createView(),
         ]);
     }
 }
--- portfolio/src/Service/ProjectService.php
+++ portfolio/src/Service/ProjectService.php
@@ -1,14 +1,51 @@
 <?php
+
+// src/Service/ProjectService.php
+
 namespace App\Service;
 
 class ProjectService
 {
+    private array $projects = [
+        [
+            'title' => 'Projet 1',
+            'slug' => 'projet-1',
+            'description' => 'Une application web super stylée.',
+            'image' => 'https://i.imgur.com/yWcPc0z.png', // 🔄
+            'dateCreated' => '2023-10-10 12:00:00',
+            'category' => 'Web',
+        ],
+        [
+            'title' => 'Projet 2',
+            'slug' => 'projet-2',
+            'description' => 'Un outil de gestion de tâches en Symfony.',
+            'image' => 'https://i.imgur.com/c5O7puZ.png', // 🔄
+            'dateCreated' => '2024-02-01 14:00:00',
+            'category' => 'Symfony',
+        ],
+        [
+            'title' => 'Projet 3',
+            'slug' => 'projet-3',
+            'description' => 'Application mobile en Flutter avec design futuriste.',
+            'image' => 'projet3.png', // 🔄
+            'dateCreated' => '2025-01-20 09:30:00',
+            'category' => 'Mobile',
+        ],
+    ];
+
     public function getAllProjects(): array
     {
-        return [
-            ['title' => 'Projet 1', 'description' => 'Description du projet 1'],
-            ['title' => 'Projet 2', 'description' => 'Description du projet 2'],
-            ['title' => 'Projet 3', 'description' => 'Description du projet 3'],
-        ];
+        return $this->projects;
+    }
+
+    public function getProjectBySlug(string $slug): ?array
+    {
+        foreach ($this->projects as $project) {
+            if ($project['slug'] === $slug) {
+                return $project;
+            }
+        }
+
+        return null;
     }
 }
